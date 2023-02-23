b101509ed10beb09385255c1c5297520c2a75740
Laraleeny <lkozlova1428@gmail.com>
2023-02-23 14:37:08 +0200

    feat: add condition if no params passed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    for (let i = 0; i < matrix.length; i++) {
+  let newArr = [];
+
+    if (matrix === undefined) {
+      return newArr;
+   }
+
+  for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       let columnIdx = i % 2 === 0 
         ? j
         : (matrix[i].length - j - 1);
         
-        return matrix[i][columnIdx];
+      newArr.push(matrix[i][columnIdx]);
     }
   }
+  return newArr;
 }
